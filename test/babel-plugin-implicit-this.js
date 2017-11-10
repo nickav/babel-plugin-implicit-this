6f74bebb563dc73185d517b3c2c44c33902c84b9
Nick Aversano <nick23hi@gmail.com>
2017-11-10 12:00:06 -0500

    add tests

--- test/babel-plugin-implicit-this.js
+++ test/babel-plugin-implicit-this.js
@@ -1,10 +1,44 @@
 const babel = require('babel-core')
 const plugin = require('../lib')
 const { expect } = require('chai')
-const transform = code => babel.transform(code, {plugins: [plugin]})
+const transform = source => babel.transform(source, {plugins: [plugin]})
 
 describe('babel-plugin-implicit-this', () => {
-  it('Transforms undefined variables', () => {
-    expect(transform('console.log(a)')).to.eq('console.log(this.a)')
+  it('transforms undefined variables', () => {
+    expect(transform('a;').code).to.eq('this.a;')
+  })
+
+  it('should transform basic assignments', () => {
+    expect(transform('x = 10;').code).to.eq('this.x = 10;')
+  })
+
+  it('should rewrite variables inside functions', () => {
+    const code = `function f() { return hello + world; }`
+    expect(transform(code).code).to.eq(`function f() { return this.hello + this.world; }`)
+  })
+
+  it('should not rewrite function short hand', () => {
+    const code = `const x = { create() { console.log('hey'); } }`
+    expect(transform(code).code).to.eq(code)
+  })
+
+  it('should not transform module.exports', () => {
+    const code = 'module.exports = {}'
+    expect(transform(code).code).to.eq(code)
+  })
+
+  it('should not transform require statements', () => {
+    const code = `const fs = require('fs')`
+    expect(transform(code).code).to.eq(code)
+  })
+
+  it('should not transform window statements', () => {
+    const code = `console.log(window.location)`
+    expect(transform(code).code).to.eq(code)
+  })
+
+  it('should not transform Object statements', () => {
+    const code = `Object.assign({}, { a: 1 })`
+    expect(transform(code).code).to.eq(code)
   })
 })
